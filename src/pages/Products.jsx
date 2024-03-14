87aad144350ab9ba4f8b4121b54b95932a082831
shri ganesh <abhiisharma76@gmail.com>
2024-03-15 03:21:16 +0530

    title error fixed

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -79,7 +79,7 @@ const Products = () => {
           </div>
         </div>
         <div>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8786.958540421263!2d-0.21507643485385738!3d51.48959473596952!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d8a00baf21de75%3A0x52963a5addd52a99!2sLondon%2C%20UK!5e0!3m2!1sen!2sin!4v1706290390705!5m2!1sen!2sin" className='w-100' height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe title='google map to navigate the address are embeded' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8786.958540421263!2d-0.21507643485385738!3d51.48959473596952!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d8a00baf21de75%3A0x52963a5addd52a99!2sLondon%2C%20UK!5e0!3m2!1sen!2sin!4v1706290390705!5m2!1sen!2sin" className='w-100' height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
     </div>
     </>
